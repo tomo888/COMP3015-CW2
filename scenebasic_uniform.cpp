cdfc0d36db3e13d180eba615a5b4f65ace46274e
tomo888 <33130687+tomo888@users.noreply.github.com>
2024-05-15 05:44:59 +0100

    PBR implemented
    Further edits to material look (roughness, metallic-ness, shininess etc) and camera angle needed, but basic implementation of PBR is present

---
+++ scenebasic_uniform.cpp
@@ -0,0 +1,149 @@
+#include "scenebasic_uniform.h"
+
+#include <cstdio>
+#include <cstdlib>
+
+#include <string>
+using std::string;
+
+#include <sstream>
+#include <iostream>
+using std::cerr;
+using std::endl;
+
+#include "helper/glutils.h"
+#include <glm/gtc/matrix_transform.hpp>
+#include "helper/texture.h"
+
+using glm::vec3;
+using glm::mat4;
+using glm::vec4;
+using glm::mat3;
+
+SceneBasic_Uniform::SceneBasic_Uniform() : mesh(ObjMesh::load("media/comp3015.obj", true)), mesh1(ObjMesh::load("media/comp3015 room.obj", true)), tPrev(0.0f), lightPos(5.0f, 5.0f, 5.0f, 1.0f) {
+
+}
+
+void SceneBasic_Uniform::initScene()
+{
+	compile();
+
+	glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
+
+	glEnable(GL_DEPTH_TEST);
+
+	view = glm::lookAt(vec3(20.0f, 1.0f, 1.0f), vec3(0.0f, 0.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f));
+
+	projection = glm::perspective(glm::radians(50.0f), (float)width / height, 0.5f, 100.f);
+
+	lightAngle = 0.0f;
+	lightRotationSpeed = 1.5f;
+
+	prog.setUniform("Light[0].L", glm::vec3(45.0f));
+	prog.setUniform("Light[0].Position", view * lightPos);
+	prog.setUniform("Light[1].L", glm::vec3(0.3f));
+	prog.setUniform("Light[1].Position", glm::vec4(0, 0.15f, -1.0f, 0));
+	prog.setUniform("Light[2].L", glm::vec3(45.0f));
+	prog.setUniform("Light[2].Position", view * glm::vec4(-7, 3, 7, 1));
+
+	GLuint texID = Texture::loadTexture("media/ImphenziaPalette02-Albedo.png");
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texID);
+}
+
+void SceneBasic_Uniform::compile()
+{
+	try {
+		prog.compileShader("shader/basic_uniform.vert");
+		prog.compileShader("shader/basic_uniform.frag");
+		prog.link();
+		prog.use();
+	} catch (GLSLProgramException &e) {
+		cerr << e.what() << endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void SceneBasic_Uniform::update( float t )
+{
+	float deltaT = t - tPrev;
+	if (tPrev == 0.0f)
+	{
+		deltaT = 0.0f;
+	}
+
+	tPrev = t;
+
+	if (animating())
+	{
+		lightAngle = glm::mod(lightAngle + deltaT * lightRotationSpeed, glm::two_pi<float>());
+		lightPos.x = glm::cos(lightAngle) * 7.0f;
+		lightPos.y = 3.0f;
+		lightPos.z = glm::sin(lightAngle) * 7.0f;
+	}
+}
+
+void SceneBasic_Uniform::render()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	prog.setUniform("Light[0].Position", view * lightPos);
+	
+	drawScene();
+}
+
+void SceneBasic_Uniform::setMatrices()
+{
+	mat4 mv = view * model;
+
+	prog.setUniform("ModelViewMatrix", mv);
+	prog.setUniform("NormalMatrix", glm::mat3(mv));
+	prog.setUniform("MVP", projection * mv);
+}
+
+void SceneBasic_Uniform::resize(int w, int h)
+{
+	glViewport(0, 0, w, h);
+
+    width = w;
+    height = h;
+
+	projection = glm::perspective(glm::radians(60.0f), (float)w / h, 0.3f, 100.0f);
+}
+
+void SceneBasic_Uniform::drawScene()
+{
+	drawRoom();
+
+	vec3 modelBaseColor(0.1f, 0.33f, 0.97f);
+	drawSpot(vec3(0.0f, 0.0f, 0.0f), 1.0f, 0, modelBaseColor);
+
+}
+
+void SceneBasic_Uniform::drawRoom()
+{
+	model = glm::mat4(1.0f);
+	model = glm::rotate(model, glm::radians(265.0f), vec3(0.0f, 1.0f, 0.0f));
+
+	prog.setUniform("Material.Rough", 0.9f);
+	prog.setUniform("Material.Metal", 0);
+	prog.setUniform("Material.Color", glm::vec3(0.2f));
+	setMatrices();
+	mesh1->render();
+
+}
+
+void SceneBasic_Uniform::drawSpot(const glm::vec3& pos, float rough, int metal, const glm::vec3& color)
+{
+	model = glm::mat4(1.0f);
+	model = glm::translate(model, vec3(0.0f, -2.0f, 0.0f));
+	model = glm::rotate(model, glm::radians(235.0f), vec3(0.0f, 1.0f, 0.0f));
+
+	prog.setUniform("Material.Rough", rough);
+	prog.setUniform("Material.Metal", metal);
+	prog.setUniform("Material.Color", color);
+
+	setMatrices();
+	mesh->render();
+
+}
---
+++ scenebasic_uniform.h
@@ -0,0 +1,35 @@
+#ifndef SCENEBASIC_UNIFORM_H
+#define SCENEBASIC_UNIFORM_H
+
+#include "helper/scene.h"
+#include <glad/glad.h>
+#include "helper/glslprogram.h"
+#include <glm/glm.hpp>
+#include "helper/objmesh.h"
+
+class SceneBasic_Uniform : public Scene
+{
+private:
+    GLSLProgram prog;
+    std::unique_ptr<ObjMesh> mesh;
+    std::unique_ptr<ObjMesh> mesh1;
+    float tPrev;
+    float lightAngle;
+    float lightRotationSpeed;
+    glm::vec4 lightPos;
+    void setMatrices();
+    void compile();
+    void drawScene();
+    void drawRoom();
+    void drawSpot(const glm::vec3& pos, float rough, int metal, const glm::vec3& color);
+
+public:
+    SceneBasic_Uniform();
+
+    void initScene();
+    void update( float t );
+    void render();
+    void resize(int, int);
+};
+
+#endif // SCENEBASIC_UNIFORM_H
